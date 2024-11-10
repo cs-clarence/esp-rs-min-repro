857037d4b4151fbf3e47fe265b010ea5d35f4a3d
Clarence Manuel <rencedm112@gmail.com>
2024-11-10 12:37:25 +0800

    make lib main public

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-fn main() {
+pub fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
